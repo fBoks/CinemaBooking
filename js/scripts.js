31fb0f6434959157286a7c1d79c2fa5dc2180ea8
Ruslan <99635646+fBoks@users.noreply.github.com>
2022-09-23 19:10:38 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+const schemeSvg = document.querySelector(".scheme-svg");
+const totalPriceTag = document.querySelector(".price-tag-total");
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
+
+let cost = 800;
+let totalPrice = 0;
+let totalSeats = 0;
+
+schemeSvg.addEventListener("click", (event) => {
+    if (!event.target.classList.contains("booked"))
+    {
+        event.target.classList.toggle("active");
+        totalSeats = schemeSvg.querySelectorAll(".active").length;
+        totalPrice = cost * totalSeats;
+        totalPriceTag.textContent = totalPrice;
+    }
+});
+
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("is-open");
+});
